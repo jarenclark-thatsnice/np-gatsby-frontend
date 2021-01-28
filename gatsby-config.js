af27c1f9bff8d78df15fa571859c336df14c4181
Jaren Clark <jaren@thatsnice.com>
2021-01-28 18:38:11 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "Nice Passion",
+  },
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};
